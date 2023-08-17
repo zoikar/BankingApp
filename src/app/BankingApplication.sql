bc73b75e1f481d91e54acc1fb88fd64f197a84c7
zoikar <134593799+zoikar@users.noreply.github.com>
2023-08-17 12:12:08 +0300

    Add files via upload

---
+++ src/app/BankingApplication.sql
@@ -0,0 +1,45 @@
+-- Database: BankingApplication
+DROP DATABASE IF EXISTS BankingApplication;
+DROP TABLE IF EXISTS CardInfo CASCADE;
+DROP TABLE IF EXISTS AccountInfo CASCADE;
+
+CREATE DATABASE BankingApplication
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1253'
+    LC_CTYPE = 'English_United States.1253'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+CREATE TABLE AccountInfo(
+	accnum INT,
+	fname varchar(100),
+	lname varchar(100),
+	mobile varchar(10),
+	id varchar(20),
+	ssn varchar(9),
+	address varchar(100),
+	email varchar(100),
+	password varchar(100),
+	primary key(accnum)
+);
+
+CREATE TABLE CardInfo(
+	accnum int,
+	cardnum varchar(16),
+	expmonth int,
+	expyear int,
+	cvv int,
+	pin int,
+	balance float,
+	primary key(cardnum),
+	FOREIGN KEY (accnum) REFERENCES AccountInfo (accnum)
+);
+
+
+
+SELECT * FROM AccountInfo;
+SELECT * FROM CardInfo;
+INSERT INTO AccountInfo(accnum, fname, lname, mobile, id, ssn, address, email, password) VALUES (2, 'Kar', 'Zoi', '6977543699', '234', '345678901', 'Somewhere', 'zoekar26@gmail.com', 'zoekar2222!');
